ab04611966fdda4c66a9d20d3ea0b00adf778c89
Joshua Pan <joshuapan8@gmail.com>
2019-11-07 03:55:15 -0500

    Add redux to lift state

---
+++ src/reducers.js
@@ -0,0 +1,23 @@
+export default function reducer(state = { todos: [] }, action) {
+  switch (action.type) {
+    case "ADD_TODO":
+      return {
+        todos: [
+          ...state.todos,
+          {
+            id: action.id,
+            text: action.text,
+            completed: false
+          }
+        ]
+      };
+    case "TOGGLE_TODO":
+      return {
+        todos: state.todos.map(todo =>
+          todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
+        )
+      };
+    default:
+      return state;
+  }
+}
